a934df13e3aa5fdfe89738ed31cc4fbe8cbcd769
randomusert <127417963+randomusert@users.noreply.github.com>
2026-03-26 19:41:49 +0200

    Register Block item for BasicTinEnergyCube; work on continues

--- common/src/main/java/io/github/randomusert/mods/tinutils/blocks/BasicTinEnergyCube.java
+++ common/src/main/java/io/github/randomusert/mods/tinutils/blocks/BasicTinEnergyCube.java
@@ -1,36 +1,32 @@
 package io.github.randomusert.mods.tinutils.blocks;
 
-import io.github.randomusert.mods.tinutils.api.block.BaseBlockEntity;
+import io.github.randomusert.mods.tinutils.api.block.BaseBlock;
 import io.github.randomusert.mods.tinutils.api.energy.EnergyStorage;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
-import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.function.Function;
 
-public class BasicTinEnergyCube extends BaseBlockEntity {
-    private final EnergyStorage energy = new EnergyStorage(10000);
+public class BasicTinEnergyCube extends BaseBlock {
+
 
     public BasicTinEnergyCube(Properties properties) {
         super(properties);
     }
 
-    @Override
-    protected InteractionResult useWithoutItem(BlockState blockState, Level level, BlockPos blockPos, Player player, BlockHitResult blockHitResult) {
-        if (!level.isClientSide) {
-            Component.literal("Energy:" + energy);
-        }
-        return super.useWithoutItem(blockState, level, blockPos, player, blockHitResult);
-    }
 
     @Override
-    public @Nullable BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
-        return new BasicTinEnergyCubeEntity(blockPos, blockState);
+    protected @NotNull InteractionResult useWithoutItem(BlockState blockState, Level level, BlockPos blockPos, Player player, BlockHitResult blockHitResult) {
+        if (!level.isClientSide() && level.getBlockEntity(blockPos) instanceof BasicTinEnergyCubeEntity cube) {
+            int energy = cube.getEnergy();
+            int capacity = cube.getCapacity();
+            player.sendSystemMessage(Component.literal("Energy: " + energy + " / " + capacity));
+        }
+        return InteractionResult.sidedSuccess(level.isClientSide());
     }
 }
--- common/src/main/java/io/github/randomusert/mods/tinutils/blocks/BasicTinEnergyCubeEntity.java
+++ common/src/main/java/io/github/randomusert/mods/tinutils/blocks/BasicTinEnergyCubeEntity.java
@@ -1,12 +1,32 @@
 package io.github.randomusert.mods.tinutils.blocks;
 
+import io.github.randomusert.mods.tinutils.api.energy.EnergyStorage;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class BasicTinEnergyCubeEntity extends BlockEntity {
-    public BasicTinEnergyCubeEntity(BlockPos blockPos, BlockState blockState) {
-        super(TinutilsBlockEntities.BASIC_TIN_ENERGY_CUBE.get(),blockPos, blockState);
+
+    private final EnergyStorage energyStorage = new EnergyStorage(10000);
+
+    public BasicTinEnergyCubeEntity(BlockPos pos, BlockState state) {
+        super(TinutilsBlockEntities.BASIC_TIN_ENERGY_CUBE.get(), pos, state);
+    }
+
+    public int getEnergy() {
+        return energyStorage.getEnergy();
+    }
+
+    public int getCapacity() {
+        return energyStorage.getCapacity();
+    }
+
+    public int insertEnergy(int amount, boolean simulate) {
+        return energyStorage.insert(amount, simulate);
+    }
+
+    public int extractEnergy(int amount, boolean simulate) {
+        return energyStorage.extract(amount, simulate);
     }
 }
